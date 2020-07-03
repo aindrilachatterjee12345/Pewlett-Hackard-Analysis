9195827ee54081756bf4b259df2bcd710daa6724
Aindrila Chatterjee <mitras@Aindrilas-iMac.attlocal.net>
2020-07-03 15:36:13 -0700

    Challenge

---
+++ Resources/Pewlett-Hackard-Analysis.sql
@@ -0,0 +1,265 @@
+---Creating tables for EmployeeDB
+
+CREATE TABLE departments (
+	dept_no VARCHAR(4) NOT NULL,
+	dept_name VARCHAR(40) NOT NULL,
+	PRIMARY KEY (dept_no),
+	UNIQUE (dept_name)
+);
+
+SELECT * FROM departments;
+
+CREATE TABLE employees (
+	emp_no INT NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
+);
+SELECT * FROM employees;
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)	
+);
+SELECT * FROM dept_manager;
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)	
+);
+SELECT * FROM dept_emp;
+CREATE TABLE titles (
+	title VARCHAR(50) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)	
+);
+
+SELECT * FROM titles;
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no,from_date)	
+);
+SELECT * FROM salaries;
+COPY departments FROM '/Users/mitras/Public/departments.csv' DELIMITER ',' CSV HEADER;
+COPY employees FROM '/Users/mitras/Public/employees.csv' DELIMITER ',' CSV HEADER;
+COPY dept_emp FROM '/Users/mitras/Public/dept_emp.csv' DELIMITER ',' CSV HEADER;
+COPY dept_manager FROM '/Users/mitras/Public/dept_manager.csv' DELIMITER ',' CSV HEADER;
+COPY salaries FROM '/Users/mitras/Public/salaries.csv' DELIMITER ',' CSV HEADER;
+COPY titles FROM '/Users/mitras/Public/titles.csv' DELIMITER ',' CSV HEADER;
+
+--Number of [titles] retiring
+--Retirement eligibility creating new tableto hold info
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+
+---Number of employee retiring 
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+---joining departments and dept_manager table
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no
+WHERE dm.to_date = ('9999-01-01');
+
+---selecting current employees
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date 
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+---Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+---Employee list with gender and salary
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+	INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01');
+	 
+SELECT * FROM emp_info;
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+SELECT * FROM manager_info;
+
+--List of employees with departments 
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name	
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+--List of Sales Employees
+
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name	
+INTO sales_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name = 'Sales';
+
+--List of Sales and Development
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name	
+INTO sales_dev
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales','Development')
+ORDER BY ce.emp_no;
+
+SELECT * FROM sales_dev;
+
+--Number of [titles] retiring
+SELECT ce.emp_no,
+		ce.first_name,
+		ce.last_name,
+		ti.title,
+		ti.from_date,
+		ti.to_date
+INTO ret_titles
+FROM current_emp as ce
+		INNER JOIN titles as ti
+			ON (ce.emp_no = ti.emp_no)
+ORDER BY ce.emp_no;
+
+--first query for challenge to download 
+SELECT * FROM ret_titles
+ORDER BY ret_titles.emp_no;
+
+----- Partition the data to show only most recent title per employee
+SELECT emp_no,
+ 	first_name,
+ 	last_name,
+ 	to_date,
+ 	title
+INTO unique_titles
+FROM
+ (SELECT emp_no,
+ 	first_name,
+ 	last_name,
+ 	to_date,
+ 	title, ROW_NUMBER() OVER
+ (PARTITION BY (emp_no)
+ ORDER BY to_date DESC) rn
+ FROM ret_titles
+ ) tmp WHERE rn = 1
+ORDER BY emp_no;
+
+--second query for challenge to download 
+SELECT * FROM unique_titles;
+
+---Counting the number of employee per title
+
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+--third query for challenge to download 
+SELECT * FROM retiring_titles;
+
+--Creating a list of employees eligible for potential mentorship program
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+	INTO mentorship
+FROM employees as e
+INNER JOIN dept_emp as de
+	ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+	ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+     AND (de.to_date = '9999-01-01')
+	 ORDER BY e.emp_no;
+
+--fourth query for challenge to download 
+SELECT * FROM mentorship;
+
+
+
+	
